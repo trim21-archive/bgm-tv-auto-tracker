16918e41d6fd0c25f38916ce297df3e09482310f
Trim21 <trim21me@gmail.com>
2018-08-11 03:59:51 +0800

    fix(userscript): subject with ep od and sp

--- userscript/src/js/App.vue
+++ userscript/src/js/App.vue
@@ -188,7 +188,8 @@ export default {
       vm.$bgmApi.getEps(this.subjectID).then(
         data => {
           let episode = vm.episode
-          let eps = data.eps.filter(val => Number.isInteger(Number(val.sort)))
+          let eps = data.eps.filter(val => Number.isInteger(Number(val.sort)) && parseInt(val.type, 10) === 0)
+
 
           eps = eps.sort(function (a, b) {
             let key = 'sort'
