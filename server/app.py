afb5d32d512a00845574e6b248fcfa15ba1f1d63
Trim21 <trim21me@gmail.com>
2018-08-27 17:26:40 +0800

    statistics missing bangumi

--- server/app.py
+++ server/app.py
@@ -17,8 +17,10 @@ from dateutil import parser
 from config import APP_ID, APP_SECRET, HOST, PROTOCOL
 from db import setup_mongo
 
+github_url = 'https://github.com/Trim21/bilibili-bangumi-tv-auto-tracker'
 callback_url = f'{PROTOCOL}://{HOST}/oauth_callback'
-oauth_url = f'https://bgm.tv/oauth/authorize?client_id={APP_ID}&response_type=code&redirect_uri={callback_url}'
+oauth_url = f'https://bgm.tv/oauth/authorize?client_id={APP_ID}' \
+            f'&response_type=code&redirect_uri={callback_url}'
 
 base_dir = pathlib.Path(path.dirname(__file__))
 
@@ -28,6 +30,7 @@ class TypeDatabase(object):
     token: motor.core.AgnosticCollection
     missing_bangumi: motor.core.AgnosticCollection
     get_collection: Callable[[str], motor.core.AgnosticCollection]
+    statistics_missing_bangumi: motor.core.AgnosticCollection
 
 
 class TypeMongoClient(object):
@@ -46,17 +49,10 @@ class WebRequest(web.Request):
 
 async def get_token(request: WebRequest, ):
     code = request.query.get('code', None)
+
     if not code:
-        return aiohttp_jinja2.render_template('post_to_extension.html', request, {'data': json.dumps({
-            "_id"          : 1,
-            "access_token" : "example_access_token",
-            "expires_in"   : 604800,
-            "token_type"   : "Bearer",
-            "scope"        : None,
-            "user_id"      : 1,
-            "refresh_token": "example_refresh_token",
-            "auth_time"    : 1529418738
-        }), })
+        raise web.HTTPFound(location=oauth_url)
+
     async with aiohttp.ClientSession() as session:
         async with session.post('https://bgm.tv/oauth/access_token',
                                 data={'grant_type'   : 'authorization_code',
@@ -72,8 +68,10 @@ async def get_token(request: WebRequest, ):
             return web.json_response(r, status=400)
         r['auth_time'] = int(parser.parse(resp.headers['Date']).timestamp())
 
-        request.app.db.token.update_one({'_id': r['user_id']}, {'$set': r}, upsert=True)
-        return aiohttp_jinja2.render_template('post_to_extension.html', request, {'data': json.dumps(r), })
+        await request.app.db.token.update_one({'_id': r['user_id']},
+                                              {'$set': r}, upsert=True)
+        return aiohttp_jinja2.render_template('post_to_extension.html', request,
+                                              {'data': json.dumps(r), })
 
 
 async def refresh_auth_token(request: WebRequest, ):
@@ -89,8 +87,8 @@ async def refresh_auth_token(request: WebRequest, ):
             'redirect_uri' : callback_url}
     print(data)
     async with aiohttp.ClientSession() as session:
-        async with session.post('https://bgm.tv/oauth/access_token', json=data) as resp:
-            # async with session.post('https://postman-echo.com/post', json=data) as resp:
+        async with session.post('https://bgm.tv/oauth/access_token',
+                                json=data) as resp:
             r = await resp.json()
         print(r)
         if 'error' in r:
@@ -98,7 +96,8 @@ async def refresh_auth_token(request: WebRequest, ):
         r['_id'] = r['user_id']
         r['auth_time'] = int(parser.parse(resp.headers['Date']).timestamp())
 
-        request.app.db.token.update_one({'_id': r['_id']}, {'$set': r}, upsert=True)
+        await request.app.db.token.update_one({'_id': r['_id']}, {'$set': r},
+                                              upsert=True)
         return web.json_response(r)
 
 
@@ -118,16 +117,23 @@ async def query_subject_id(request: WebRequest):
     website = request.query.get('website', None)
     bangumi_id = request.query.get('bangumiID', None)
     if not (website and bangumi_id and website in ['iqiyi', 'bilibili']):
-        raise web.HTTPBadRequest(reason='missing input `website` or `bangumiID`')
+        raise web.HTTPBadRequest(
+            reason='missing input `website` or `bangumiID`'
+        )
     collection = request.app.db.get_collection(website)
     e = await collection.find_one({'_id': bangumi_id})
+    await request.app.db.statistics_missing_bangumi.delete_one(
+        {'website': website, 'bangumi_id': bangumi_id},
+    )
     if e:
         return web.json_response(e)
     else:
+        await request.app.db.statistics_missing_bangumi.update_one(
+            {'website': website, 'bangumi_id': bangumi_id},
+            {'$inc': {'times': 1}},
+            upsert=True
+        )
         raise web.HTTPNotFound()
-    # request.mongo.get_data
-
-    pass
 
 
 class ReportMissingBangumiValidator(Validator):
@@ -142,17 +148,39 @@ async def report_missing_bangumi(request: WebRequest):
     data = await request.json()
     v = ReportMissingBangumiValidator(data)
     if not v.is_valid():
-        return web.json_response({'message': v.str_errors, 'code': 400, 'status': 'error'}, status=400)
+        return web.json_response(
+            {'message': v.str_errors, 'code': 400, 'status': 'error'},
+            status=400)
     data = v.validated_data
     try:
         await request.app.db.missing_bangumi.insert_one(data)
         return web.json_response({'status': 'success'}, status=201)
     except Exception as e:
-        return web.json_response({'status': 'error', 'message': str(e)}, status=502)
+        return web.json_response({'status': 'error', 'message': str(e)},
+                                 status=502)
 
 
 async def missing_bangumi(request: WebRequest):
     f = await request.app.db.missing_bangumi.find({}, {'_id': 0}).to_list(30)
+    return web.json_response(f)
+
+
+website_template = {
+    'bilibili': 'https://bangumi.bilibili.com/anime/{}',
+    'iqiyi'   : 'https://www.iqiyi.com/{}.html'
+}
+
+
+async def statistics_missing_bangumi(request: WebRequest):
+    f = await request.app.db.statistics_missing_bangumi.find({},
+                                                             {'_id': 0}
+                                                             ).to_list(
+        500)
+
+    for item in f:
+        item['url'] = website_template.get(item['website'], '{}').format(
+            item['bangumi_id']
+        )
     return web.json_response(f)
 
 
@@ -168,15 +196,19 @@ def create_app(io_loop=asyncio.get_event_loop()):
         # middlewares=[error_middleware, ]
     )
     setup_mongo(app, io_loop)
-    aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader(str(base_dir / 'templates')))
+    aiohttp_jinja2.setup(app,
+                         loader=jinja2.FileSystemLoader(
+                             str(base_dir / 'templates')
+                         ))
     app.add_routes([
-        web.get('/api/v0.1/missing_bangumi', missing_bangumi),
-        web.post('/api/v0.1/refresh_token', refresh_auth_token),
-        web.post('/api/v0.1/reportMissingBangumi', report_missing_bangumi),
-        web.get('/', redirect('https://github.com/Trim21/bilibili-bangumi-tv-auto-tracker')),
+        web.get('/', redirect(github_url)),
+        web.get('/auth', redirect(oauth_url)),
         web.get('/oauth_callback', get_token),
         web.get('/api/v0.2/querySubjectID', query_subject_id),
-        web.get('/auth', redirect(oauth_url))
+        web.get('/api/v0.1/missing_bangumi', missing_bangumi),
+        web.get('/statistics_missing_bangumi', statistics_missing_bangumi),
+        web.post('/api/v0.1/refresh_token', refresh_auth_token),
+        web.post('/api/v0.1/reportMissingBangumi', report_missing_bangumi),
     ])
     cors = aiohttp_cors.setup(app, defaults={
         "*": aiohttp_cors.ResourceOptions(
