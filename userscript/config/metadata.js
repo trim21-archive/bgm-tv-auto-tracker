113494c9f8f03aa964bc11a4e0159bc74b78d9c8
Trim21 <niuziru21@gmail.com>
2018-10-02 02:40:27 +0800

    fix: require wrong axios build file

--- userscript/config/metadata.js
+++ userscript/config/metadata.js
@@ -15,7 +15,7 @@ module.exports = {
   ],
   'require': [
     'https://cdn.jsdelivr.net/npm/jquery@3.3.1/dist/jquery.min.js',
-    'https://cdn.jsdelivr.net/npm/axios@0.18.0/index.min.js',
+    'https://cdn.jsdelivr.net/npm/axios@0.18.0/dist/axios.min.js',
     'https://cdn.jsdelivr.net/npm/axios-userscript-adapter@0.0.3/dist/axiosGmxhrAdapter.min.js',
     'https://cdn.jsdelivr.net/npm/vue@2.5.17/dist/vue.min.js'
   ],
