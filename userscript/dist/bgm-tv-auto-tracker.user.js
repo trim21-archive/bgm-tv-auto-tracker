62909fdb86222afa4028e456f030090a6bcbf9d3
Trim21 <niuziru21@gmail.com>
2019-01-08 23:26:39 +0800

    v0.10.0

--- userscript/dist/bgm-tv-auto-tracker.user.js
+++ userscript/dist/bgm-tv-auto-tracker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Bgm.tv auto tracker
 // @namespace   https://trim21.me/
-// @version     0.9.9
+// @version     0.10.0
 // @author      Trim21 <trim21me@gmail.com>
 // @source      https://github.com/Trim21/bilibili-bangumi-tv-auto-tracker
 // @license     MIT
@@ -1139,9 +1139,21 @@ class website_bilibili {
     let title = status.mediaInfo.title
     let episodeStartWith = parseInt(status.epList[0].index)
 
+    apiServer.post('/api/v0.1/collect_episode_info', {
+      mediaInfo: gmUnsafeWindow.__INITIAL_STATE__.mediaInfo,
+      epInfo: gmUnsafeWindow.__INITIAL_STATE__.epInfo,
+      epList: gmUnsafeWindow.__INITIAL_STATE__.epList,
+    }).then(
+      res => {
+        console.log(res)
+      },
+      err => {
+        console.log(err)
+      })
+
     return new Promise((resolve, reject) => {
       apiServer.get('/api/v0.2/querySubjectID', {
-        params: { bangumiID, website: 'bilibili' }
+        params: {bangumiID, website: 'bilibili'}
       }).then(
         response => resolve({
           subjectID: response.data.subject_id,
@@ -1167,7 +1179,7 @@ class website_bilibili {
     let bangumiID = status.mediaInfo.season_id
     let INNER_EPISODE = gmUnsafeWindow.__INITIAL_STATE__.epInfo.index
 
-    function onEpisodeChange ({ season = false, episode = false }) {
+    function onEpisodeChange ({season = false, episode = false}) {
       if (season) {
         cls.init().then(
           data => {
@@ -1240,7 +1252,7 @@ class website_iQiyi {
 
     return new Promise((resolve, reject) => {
       apiServer.get('/api/v0.2/querySubjectID', {
-        params: { bangumiID, website: 'iqiyi' }
+        params: {bangumiID, website: 'iqiyi'}
       }).then(
         response => {
           console.log(response)
@@ -1267,7 +1279,7 @@ class website_iQiyi {
       let title = gmUnsafeWindow.document.title
       let episode = parseEpisode(title)
       if (episode) {
-        cb({ episode })
+        cb({episode})
       } else {
         notfound()
       }
@@ -1956,7 +1968,7 @@ exports = module.exports = __webpack_require__("I1BE")(false);
 
 
 // module
-exports.push([module.i, "\n#bgm_tv_tracker.disable .bgm_tv_tracker_info{display:none\n}\ninput[type=checkbox]{-webkit-appearance:checkbox;-moz-appearance:checkbox;appearance:checkbox\n}\n.iqiyi #bgm_tv_tracker_btn_on_page{color:#797979;border-top-left-radius:5px;border-top-right-radius:5px;cursor:pointer;display:block;font-weight:700;font-family:PingFangSC-Regular,Helvetica,Arial,Microsoft Yahei,sans-serif;font-size:16px;font-stretch:100%;font-style:normal;font-variant-caps:normal;font-variant-east-asian:normal;-webkit-font-variant-ligatures:normal;font-variant-ligatures:normal;font-variant-numeric:normal;height:38px;line-height:38px;margin:0;max-width:77px;overflow-x:hidden;overflow-y:hidden;padding:0 10px\n}\n.iqiyi#bgm_tv_tracker{margin-left:15px;padding-left:16px;position:relative;font-size:15px;float:left;cursor:pointer;display:inline\n}\n.iqiyi #bgm_tv_tracker_link a{color:#000\n}\n.iqiyi#bgm_tv_tracker .bgm_tv_tracker_info{opacity:1;pointer-events:auto;top:100%\n}\n.iqiyi .bgm_tv_tracker_btn.bgm_tv_tracker{-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none;float:left;cursor:pointer;font-size:14px;height:28px;line-height:18px;text-align:center;width:100px!important;-webkit-transition:all .1s ease-in;transition:all .1s ease-in\n}\n.iqiyi .bgm_tv_tracker_info{padding:8px;margin-top:5px;background:#fff;border:1px solid #e5e9ef;cursor:default;height:auto;left:-1px;line-height:normal;opacity:0;pointer-events:none;position:absolute;text-align:left;top:70px;white-space:normal;width:250px;z-index:1000\n}\n.iqiyi .bgm_tv_tracker_info *{max-width:100%\n}\n.iqiyi .bgm_tv_tracker_info button{padding:4px 6px;line-height:14px;display:inline-block;margin:4px\n}\n#bangumi_detail .bangumi-info.clearfix .info-right .info-title.clearfix a h2{width:380px\n}\n@media screen and (max-width:1400px){\n.arc-toolbar .block{padding:0 12px;margin-left:-12px\n}\n.video-toolbar-module .btn-item{padding:0 0 0 60px!important;margin-left:-12px\n}\n#bangumi_detail .bangumi-info.clearfix .info-right .info-title.clearfix a h2{width:200px!important\n}\n}\n.bilibili#bgm_tv_tracker{display:inline-block;position:relative;float:left;margin-right:20px\n}\n.bilibili .bgm_tv_tracker_radius{border-radius:4px;border:1px solid #e5e9ef\n}\n.bilibili .bgm_tv_tracker_btn.bgm_tv_tracker:hover{color:#00a1d6;border:1px solid #00a1d6\n}\n.bilibili .bgm_tv_tracker_btn.bgm_tv_tracker{-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none;color:#6d757a;float:left;cursor:pointer;font-size:14px;height:28px;line-height:28px;text-align:center;width:100px!important;-webkit-transition:all .1s ease-in;transition:all .1s ease-in\n}\n.bilibili .bgm_tv_tracker_info{padding:8px;margin-top:5px;background:#fff;border-radius:0 0 4px 4px;border:1px solid #e5e9ef;-webkit-box-shadow:0 2px 4px rgba(0,0,0,.16);box-shadow:0 2px 4px rgba(0,0,0,.16);cursor:default;height:auto;left:-1px;line-height:normal;opacity:0;pointer-events:none;position:absolute;text-align:left;top:70px;white-space:normal;width:300px;z-index:1000\n}\n.bilibili .bgm_tv_tracker_info *{max-width:100%\n}\n.bilibili .bgm_tv_tracker_info{opacity:1;pointer-events:auto;top:100%\n}\n.bilibili .bgm_tv_tracker_info button{padding:4px 6px;line-height:14px;display:inline-block;margin:4px;border:2px solid #fff\n}\n.bilibili .bgm_tv_tracker_info button:active{background:#fff\n}\n.bilibili .bgm_tv_tracker_info button:hover{border:2px solid #99bdf7\n}", ""]);
+exports.push([module.i, "\n#bgm_tv_tracker.disable .bgm_tv_tracker_info{display:none\n}\ninput[type=checkbox]{-webkit-appearance:checkbox;-moz-appearance:checkbox;appearance:checkbox\n}\n.iqiyi #bgm_tv_tracker_btn_on_page{color:#797979;border-top-left-radius:5px;border-top-right-radius:5px;cursor:pointer;display:block;font-weight:700;font-family:PingFangSC-Regular,Helvetica,Arial,Microsoft Yahei,sans-serif;font-size:16px;font-stretch:100%;font-style:normal;font-variant-caps:normal;font-variant-east-asian:normal;-webkit-font-variant-ligatures:normal;font-variant-ligatures:normal;font-variant-numeric:normal;height:38px;line-height:38px;margin:0;white-space:nowrap;overflow-x:hidden;overflow-y:hidden;padding:0 10px\n}\n.iqiyi#bgm_tv_tracker{margin-left:15px;padding-left:16px;position:relative;font-size:15px;float:left;cursor:pointer;display:inline\n}\n.iqiyi #bgm_tv_tracker_link a{color:#000\n}\n.iqiyi#bgm_tv_tracker .bgm_tv_tracker_info{opacity:1;pointer-events:auto;top:100%\n}\n.iqiyi .bgm_tv_tracker_btn.bgm_tv_tracker{-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none;float:left;cursor:pointer;font-size:14px;height:28px;line-height:18px;text-align:center;width:100px!important;-webkit-transition:all .1s ease-in;transition:all .1s ease-in\n}\n.iqiyi .bgm_tv_tracker_info{padding:8px;margin-top:5px;background:#fff;border:1px solid #e5e9ef;cursor:default;height:auto;left:-1px;line-height:normal;opacity:0;pointer-events:none;position:absolute;text-align:left;top:70px;white-space:normal;width:250px;z-index:1000\n}\n.iqiyi .bgm_tv_tracker_info *{max-width:100%\n}\n.iqiyi .bgm_tv_tracker_info button{padding:4px 6px;line-height:14px;display:inline-block;margin:4px\n}\n#bangumi_detail .bangumi-info.clearfix .info-right .info-title.clearfix a h2{width:380px\n}\n@media screen and (max-width:1400px){\n.arc-toolbar .block{padding:0 12px;margin-left:-12px\n}\n.video-toolbar-module .btn-item{padding:0 0 0 60px!important;margin-left:-12px\n}\n#bangumi_detail .bangumi-info.clearfix .info-right .info-title.clearfix a h2{width:200px!important\n}\n}\n.bilibili#bgm_tv_tracker{display:inline-block;position:relative;float:left;margin-right:20px\n}\n.bilibili .bgm_tv_tracker_radius{border-radius:4px;border:1px solid #e5e9ef\n}\n.bilibili .bgm_tv_tracker_btn.bgm_tv_tracker:hover{color:#00a1d6;border:1px solid #00a1d6\n}\n.bilibili .bgm_tv_tracker_btn.bgm_tv_tracker{-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none;color:#6d757a;float:left;cursor:pointer;font-size:14px;height:28px;line-height:28px;text-align:center;width:100px!important;-webkit-transition:all .1s ease-in;transition:all .1s ease-in\n}\n.bilibili .bgm_tv_tracker_info{padding:8px;margin-top:5px;background:#fff;border-radius:0 0 4px 4px;border:1px solid #e5e9ef;-webkit-box-shadow:0 2px 4px rgba(0,0,0,.16);box-shadow:0 2px 4px rgba(0,0,0,.16);cursor:default;height:auto;left:-1px;line-height:normal;opacity:0;pointer-events:none;position:absolute;text-align:left;top:70px;white-space:normal;width:300px;z-index:1000\n}\n.bilibili .bgm_tv_tracker_info *{max-width:100%\n}\n.bilibili .bgm_tv_tracker_info{opacity:1;pointer-events:auto;top:100%\n}\n.bilibili .bgm_tv_tracker_info button{padding:4px 6px;line-height:14px;display:inline-block;margin:4px;border:2px solid #fff\n}\n.bilibili .bgm_tv_tracker_info button:active{background:#fff\n}\n.bilibili .bgm_tv_tracker_info button:hover{border:2px solid #99bdf7\n}", ""]);
 
 // exports
 
